c32daa6b39cdeb0e6e3f315f849082d3086824dc
Antonio <contact@goweki.com>
2024-07-23 00:42:02 +0300

    ...

--- src/components/menubars.tsx
+++ src/components/menubars.tsx
@@ -183,7 +183,7 @@ export function NavigationMenuComponent() {
           <NavigationMenuTrigger>Getting started</NavigationMenuTrigger>
           <NavigationMenuContent>
             <ul className="grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
-              <li className="row-span-3">
+              {/* <li className="row-span-3">
                 <NavigationMenuLink asChild>
                   <a
                     className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
@@ -200,7 +200,7 @@ export function NavigationMenuComponent() {
                     </p>
                   </a>
                 </NavigationMenuLink>
-              </li>
+              </li> */}
               <ListItem href="/docs" title="Introduction">
                 Re-usable components built using Radix UI and Tailwind CSS.
               </ListItem>
