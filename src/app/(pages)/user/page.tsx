b57e6b81c363a02d579b6538e9879d1a2a5af72c
Antonio <contact@goweki.com>
2024-07-22 16:09:19 +0300

    ...

--- src/app/(pages)/user/page.tsx
+++ src/app/(pages)/user/page.tsx
@@ -53,7 +53,7 @@ export default function UserHomePage() {
   return authStatus === "loading" || !userSession?.user ? (
     <LoaderHourglass isLoading={authStatus === "loading"} />
   ) : (
-    <main className="grid flex-1 items-start gap-4 sm:p-4 sm:px-6 sm:py-0 md:gap-8 lg:grid-cols-3 xl:grid-cols-3">
+    <main className="grid flex-1 items-start gap-4 p-2 sm:px-6 sm:py-0 md:gap-8 lg:grid-cols-3 xl:grid-cols-3">
       <div className="grid auto-rows-max items-start gap-4 md:gap-8 lg:col-span-2">
         <div className="grid gap-4 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-2 xl:grid-cols-4">
           <Card className="sm:col-span-2" x-chunk="dashboard-05-chunk-0">
