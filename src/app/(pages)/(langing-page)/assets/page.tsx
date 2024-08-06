a7ed2080204e1136cd6b0195cedea8d740f64700
Antonio <contact@goweki.com>
2024-08-06 10:42:19 +0300

    202407061042

--- src/app/(pages)/(langing-page)/assets/page.tsx
+++ src/app/(pages)/(langing-page)/assets/page.tsx
@@ -1,33 +1,11 @@
 "use client";
 import { useContext, useEffect, useState } from "react";
 import { DataContext } from "@/components/providers";
-import { TableViewLayout } from "@/components/pages/table";
 import { Asset, User } from "@/lib/prisma/types";
 
 export default function LegislationsPage() {
   const { assets } = useContext(DataContext);
 
   // render
-  return (
-    <TableViewLayout
-      title="Legislations"
-      link="Legislations"
-      desc="Bills and Acts of Kenyan Parliament"
-      items={assets.map(({ id, title, description, status, type }) => ({
-        id,
-        title,
-        description,
-        status,
-        type,
-      }))}
-      headers={[
-        "Icon",
-        "Legislation",
-        "Description",
-        "Status",
-        "Type",
-        "Contributions",
-      ]}
-    />
-  );
+  return <main>Assets</main>;
 }
--- src/app/(pages)/(langing-page)/users/page.tsx
+++ src/app/(pages)/(langing-page)/users/page.tsx
@@ -1,24 +1,10 @@
 "use client";
 import { useContext, useEffect, useState } from "react";
 import { DataContext } from "@/components/providers";
-import { TableViewLayout } from "@/components/pages/table";
 import { Asset, User } from "@/lib/prisma/types";
-export default function MpsPage() {
+export default function UsersPage() {
   const { users } = useContext(DataContext);
 
   // render
-  return (
-    <TableViewLayout
-      title="Members of Parliament"
-      link="Mps"
-      desc="List of the elected Members of the Kenyan Parliament as sourced from parliament.co.ke"
-      items={users.map(({ id, avatar, name, assets }) => ({
-        id,
-        avatar,
-        name,
-        assets,
-      }))}
-      headers={["Photo", "Name", "Assets"]}
-    />
-  );
+  return <main>Users</main>;
 }
