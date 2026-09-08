f3278f6b7c9ad7a84631fde19f354d34b5046082
Goweki <contact@goweki.com>
2026-09-08 19:06:17 +0300

    ....

--- src/lib/prisma/scripts/backup.ts
+++ src/lib/prisma/scripts/backup.ts
@@ -4,7 +4,7 @@ import { promisify } from "util";
 import path from "path";
 import fs from "fs";
 
-dotenv.config({ path: ".env.local" });
+dotenv.config({ path: ".env.production" });
 
 const execFileAsync = promisify(execFile);
 
